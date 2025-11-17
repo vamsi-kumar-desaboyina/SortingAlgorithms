7ef5a1f5a4827b2ac04ce5fbbf2c43cbc372198a
vamsi-kumar-desaboyina <desaboyinavamsi@gmail.com>
2025-11-17 23:06:36 +0530

    insertionsort

---
+++ insertionsort/www/InsertionSort.java
@@ -0,0 +1,29 @@
+package com.insertionsort.www;
+
+import java.util.*;
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr={6,7,9,7,8,1,2};
+        for(int i=0;i<arr.length-1;i++)
+        {
+            for(int j=i+1;j>0;j--)
+            {
+                if(arr[j]<arr[j-1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+//                    System.out.println(Arrays.toString(arr));
+                }
+                else
+                {
+                    break;
+                }
+                
+            }
+            
+        }
+        System.out.println(Arrays.toString(arr));
+        
+    }
+}
